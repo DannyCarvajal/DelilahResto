507aeb02c4180b18f6b3e3698c3eb7a65b18c8cc
tsweetheart <juan-daniel1914@hotmail.com>
2021-05-27 19:33:29 -0500

    finished project

--- src/model/dbInitialize.js
+++ src/model/dbInitialize.js
@@ -6,6 +6,9 @@ const { DataTypes } = Sequelize
 /* DATABASE CONNECTION */
 import connection from '../database.js'
 
+/* INITIAL DATA */
+import {defaultValues} from './seeds.js'
+
 //* RELATIONS *//
 import { roleModel } from './Role.js'
 import { userModel } from './User.js'
@@ -22,7 +25,9 @@ const order = orderModel(connection, DataTypes)
 const orderplate = orderplateModel(connection, DataTypes)
 
 /* --------- DB SYNC AND INITIAL DATA--------------- */
-export default connection.sync()
+export default connection.sync({force: true})
+    .then(()=> defaultValues())
+    .catch(err=> console.log(err))
 
 export { role, user, state, plate, order, orderplate }
 
